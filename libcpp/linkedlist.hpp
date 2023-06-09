843de9c4349f099a1a90104e262cd760f3122b04
sleo <leo.sautron@epitech.eu>
2023-06-09 10:16:37 +0400

    feat: add libcpp and start linkedlist

---
+++ libcpp/linkedlist.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** libcpp
+** File description:
+** linkedlist
+*/
+
+#ifndef LINKEDLIST_HPP_
+#define LINKEDLIST_HPP_
+
+#include <iostream>
+#include <memory>
+#include <utility>
+
+
+#endif /* !LINKEDLIST_HPP_ */
